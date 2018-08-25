2cbae4eacc70049f38ac03539660aea26963e23b
Nirmalya Ghosh <nirmalya.email@gmail.com>
2018-08-25 23:02:42 +0530

    feat(app): Add examples boilerplate

---
+++ examples/src/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import SearchBox from "react-search-box";
+
+class App extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default App;
---
+++ examples/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
