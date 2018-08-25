ccc6a2d7cf5515357e35bbcad437a0653e9a7fa0
Nirmalya Ghosh <nirmalya.email@gmail.com>
2018-08-25 22:56:14 +0530

    feat(app): Use <Downshift />
    - Completely rewrite the whole package
    - Use <Downshift />
    - Upgrade packages

--- src/index.js
+++ src/index.js
@@ -1,137 +1,66 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import _filter from 'lodash/filter';
-import classNames from 'classnames';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import Downshift from "downshift";
 
-import Dropdown from './dropdown';
+const items = [
+  { value: "apple" },
+  { value: "pear" },
+  { value: "orange" },
+  { value: "grape" },
+  { value: "banana" }
+];
 
-const InputWrapper = styled.input`
-  &.input {
-    border: 1px solid #f1f1f1;
-    border-radius: 3px;
-    padding: 10px;
-    font-size: 16px;
-    font-weight: 400;
-    outline: 0;
-    width: 100%;
-    box-sizing: border-box;
-
-    &--show-dropdown {
-      border-bottom-right-radius: 0;
-      border-bottom-left-radius: 0;
-    }
-  }
-`;
-
-export default class Search extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      matchedData: [],
-      value: '',
-      showDropdown: false
-    };
-  }
-
-  static propTypes = {
-    placeholder: PropTypes.string,
-    exactSearch: PropTypes.bool,
-    onChange: PropTypes.func,
-    class: PropTypes.string,
-    searchKey: PropTypes.string
-  };
-
-  static defaultProps = {
-    placeholder: 'Search...',
-    data: [],
-    exactSearch: false,
-    class: ''
-  };
-
-  handleChange(e) {
-    if (e.target.value.trim()) {
-      let matchedData = this.getValueByKey(e.target.value.trim(), this.props.data);
-
-      this.setState({
-        matchedData,
-        value: e.target.value,
-        showDropdown: true
-      });
-    } else {
-      this.setState({
-        value: e.target.value,
-        showDropdown: false
-      });
-    }
-  }
-
-  getValueByKey(key, array) {
-    if (this.props.exactSearch) {
-      return this.exactSearchQuery(key, array);
-    }
-
-    return this.fuzzySearchQuery(key, array);
-  }
-
-  exactSearchQuery(key, array) {
-    let _this = this;
-
-    return _filter(array, { [`${_this.props.searchKey}`]: key });
-  }
-
-  fuzzySearchQuery(key, array) {
-    let _this = this;
-
-    return _filter(array, (item) => {
-      let searchableItem = item[`${_this.props.searchKey}`].toString();
-      if (searchableItem.indexOf(key) !== -1) {
-        return searchableItem;
-      }
-    });
-  }
-
-  handleSetValue(value) {
-    this.setState({
-      value: value.target.innerText,
-      showDropdown: false
-    }, () => {
-      if (this.props.onChange) {
-        this.props.onChange(this.state.value);
-      }
-    });
-  }
-
+class Select extends Component {
   render() {
     return (
-      <div
-        className={ classNames({
-          search: true,
-          [this.props.class]: !this.props.class ? false : true
-        }) }
+      <Downshift
+        onChange={selection => alert(`You selected ${selection.value}`)}
+        itemToString={item => (item ? item.value : "")}
       >
-        <div className="search__input">
-          <InputWrapper
-            type="text"
-            className={ classNames({
-              input: true,
-              'input--show-dropdown': this.state.showDropdown
-            }) }
-            placeholder={ this.props.placeholder }
-            onChange={ this.handleChange.bind(this) }
-            value={ this.state.value }
-          />
-        </div>
-        <div className="search__dropdown">
-          <Dropdown
-            data={ this.state.matchedData }
-            onClick={ this.handleSetValue.bind(this) }
-            show={ this.state.showDropdown }
-            searchKey={ this.props.searchKey }
-          />
-        </div>
-      </div>
+        {({
+          getInputProps,
+          getItemProps,
+          getLabelProps,
+          getMenuProps,
+          isOpen,
+          inputValue,
+          highlightedIndex,
+          selectedItem
+        }) => (
+          <div>
+            <label {...getLabelProps()}>Enter a fruit</label>
+            <input {...getInputProps()} />
+            <ul {...getMenuProps()}>
+              {isOpen
+                ? items
+                    .filter(
+                      item => !inputValue || item.value.includes(inputValue)
+                    )
+                    .map((item, index) => (
+                      <li
+                        {...getItemProps({
+                          key: item.value,
+                          index,
+                          item,
+                          style: {
+                            backgroundColor:
+                              highlightedIndex === index
+                                ? "lightgray"
+                                : "white",
+                            fontWeight:
+                              selectedItem === item ? "bold" : "normal"
+                          }
+                        })}
+                      >
+                        {item.value}
+                      </li>
+                    ))
+                : null}
+            </ul>
+          </div>
+        )}
+      </Downshift>
     );
   }
 }
+
+export default Select;
