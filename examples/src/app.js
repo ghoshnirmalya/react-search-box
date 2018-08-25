99fb592829a1fc95fddd1759b1d7e4fec681e957
Nirmalya Ghosh <nirmalya.email@gmail.com>
2018-08-26 01:49:44 +0530

    feat(app): Style component using styled-components

--- examples/src/app.js
+++ examples/src/app.js
@@ -1,9 +1,44 @@
 import React, { Component } from "react";
-import SearchBox from "react-search-box";
+import SearchBox from "./playground";
 
 class App extends Component {
+  state = {
+    data: [],
+    loading: false
+  };
+
+  componentDidMount = () => {
+    this.setState({
+      loading: true
+    });
+
+    fetch("https://api.github.com/search/repositories?q=topic:ruby+topic:rails")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          data: data.items,
+          loading: false
+        });
+      });
+  };
+
+  handleChange = selection => {
+    selection ? console.log(selection.full_name) : console.log("reverted");
+  };
+
   render() {
-    return <div />;
+    return (
+      <SearchBox
+        data={this.state.data}
+        onChange={this.handleChange}
+        placeholder="Search for a string..."
+        class="search-class"
+        searchKey="full_name"
+        loading={this.state.loading}
+        width={300}
+        height={40}
+      />
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,63 @@
 import React, { Component } from "react";
 import Downshift from "downshift";
-
-const items = [
-  { value: "apple" },
-  { value: "pear" },
-  { value: "orange" },
-  { value: "grape" },
-  { value: "banana" }
-];
+import styled from "styled-components";
 
 class Select extends Component {
+  static defaultProps = {
+    width: 250,
+    height: 30
+  };
+
+  static StyledContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+  `;
+
+  static StyledLabel = styled.label`
+    margin-bottom: 5px;
+  `;
+
+  static StyledInput = styled.input`
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+    height: ${props => props.height}px;
+    width: ${props => props.width}px;
+    border-radius: 2px;
+    font-size: inherit;
+    padding: 5px 10px;
+    background-color: ${props => (props.disabled ? "#eee" : "transparent")};
+  `;
+
+  static StyledMenu = React.forwardRef((props, ref) => (
+    <Select.StyledUl innerRef={ref} {...props} />
+  ));
+
+  static StyledUl = styled.ul`
+    box-sizing: border-box;
+    width: ${props => props.width}px;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  `;
+
+  static StyledLi = styled.li`
+    box-sizing: border-box;
+    height: ${props => props.height}px;
+    border: 1px solid #ccc;
+    padding: 5px 10px;
+
+    &:not(:first-child) {
+      border-top: 0;
+      display: flex;
+      align-items: center;
+    }
+  `;
+
   render() {
     return (
       <Downshift
-        onChange={selection => alert(`You selected ${selection.value}`)}
-        itemToString={item => (item ? item.value : "")}
+        onChange={selection => this.props.onChange(selection)}
+        itemToString={item => (item ? item[this.props.searchKey] : "")}
       >
         {({
           getInputProps,
@@ -23,40 +66,44 @@ class Select extends Component {
           getMenuProps,
           isOpen,
           inputValue,
-          highlightedIndex,
-          selectedItem
+          getRootProps
         }) => (
-          <div>
-            <label {...getLabelProps()}>Enter a fruit</label>
-            <input {...getInputProps()} />
-            <ul {...getMenuProps()}>
+          <Select.StyledContainer
+            {...getRootProps({ refKey: "downshift" })}
+            className={this.props.class}
+          >
+            <Select.StyledLabel {...getLabelProps()}>
+              {this.props.placeholder}
+            </Select.StyledLabel>
+            <Select.StyledInput
+              loading={this.props.loading}
+              height={this.props.height}
+              width={this.props.width}
+              {...getInputProps({ disabled: this.props.loading })}
+            />
+            <Select.StyledMenu {...getMenuProps()} width={this.props.width}>
               {isOpen
-                ? items
+                ? this.props.data
                     .filter(
-                      item => !inputValue || item.value.includes(inputValue)
+                      item =>
+                        !inputValue ||
+                        item[this.props.searchKey].includes(inputValue)
                     )
                     .map((item, index) => (
-                      <li
+                      <Select.StyledLi
                         {...getItemProps({
-                          key: item.value,
+                          key: item[this.props.searchKey],
                           index,
-                          item,
-                          style: {
-                            backgroundColor:
-                              highlightedIndex === index
-                                ? "lightgray"
-                                : "white",
-                            fontWeight:
-                              selectedItem === item ? "bold" : "normal"
-                          }
+                          item
                         })}
+                        height={this.props.height}
                       >
-                        {item.value}
-                      </li>
+                        {item[this.props.searchKey]}
+                      </Select.StyledLi>
                     ))
                 : null}
-            </ul>
-          </div>
+            </Select.StyledMenu>
+          </Select.StyledContainer>
         )}
       </Downshift>
     );
