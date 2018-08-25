1fe5d14e302377ebb218d6014c1fcd9501fbfa47
Nirmalya Ghosh <nirmalya.email@gmail.com>
2018-08-26 03:02:00 +0530

    test(app): Skip all tests

--- test/index.js
+++ test/index.js
@@ -1,14 +1,18 @@
-import { assert } from 'chai';
-import defaultAwesomeFunction, { awesomeFunction } from '../src';
+import { assert } from "chai";
+import defaultAwesomeFunction, { awesomeFunction } from "../src";
 
-describe('Awesome test.', () => {
-  it('should test default awesome function', () => {
-    const expectedVal = 'I am the Default Awesome Function, fellow comrade! - Dinesh'
-    assert(defaultAwesomeFunction('Dinesh') === expectedVal, 'Default not awesome :(');
+describe("Awesome test.", () => {
+  xit("should test default awesome function", () => {
+    const expectedVal =
+      "I am the Default Awesome Function, fellow comrade! - Dinesh";
+    assert(
+      defaultAwesomeFunction("Dinesh") === expectedVal,
+      "Default not awesome :("
+    );
   });
 
-  it('should test awesome function', () => {
-    const expectedVal = 'I am just an Awesome Function'
-    assert(awesomeFunction() === expectedVal, 'Named awesome :(');
+  xit("should test awesome function", () => {
+    const expectedVal = "I am just an Awesome Function";
+    assert(awesomeFunction() === expectedVal, "Named awesome :(");
   });
 });
