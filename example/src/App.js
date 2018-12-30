cf02aab34fe38ca096acb23c2e1f4ab253b68251
Nirmalya Ghosh <nirmalya.email@gmail.com>
2018-12-30 11:49:06 +0530

    refactor(app): Adds & configures Prettier

--- example/src/App.js
+++ example/src/App.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react'
 import ExampleComponent from 'react-search-box'
 
 export default class App extends Component {
-  render () {
+  render() {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ExampleComponent text="Modern React component module" />
       </div>
     )
   }
