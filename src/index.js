929d6848ec9715d0443f4f3f8ee2d0ed486e6a67
Nirmalya Ghosh <nirmalya.email@gmail.com>
2018-12-30 23:53:28 +0530

    refactor(app): Remove defaultValue attribute from <input />

--- src/index.js
+++ src/index.js
@@ -151,7 +151,6 @@ export default class ReactSearchBox extends Component {
         className={styles.input}
         type="text"
         placeholder={placeholder}
-        defaultValue={value}
         value={value}
         onChange={this.handleInputChange}
       />
