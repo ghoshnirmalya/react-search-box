58c6ff5f37622d3a2f82ca1560a5c8f146ea4a62
Nirmalya Ghosh <nirmalya.email@gmail.com>
2021-10-17 23:44:49 +0530

    chore: removes monorepo

--- example/index.tsx
+++ example/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ReactSearchBox from "@react-search-box/core";
+import ReactSearchBox from "../src";
 
 ReactDOM.render(
   <React.StrictMode>
